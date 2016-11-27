3fb4db51768370013b51f36a10c3ca09a2f66efa
vu-be <vube@berkeley.edu>
2016-11-27 01:30:14 -0800

    clear

--- assets/js/script_guild_about.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	$(".guild_about :not(.section_wrapper)").hide().fadeIn(700, "swing");
-
-});
--- assets/js/script_member_home.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-	$("#member_header, .section_wrapper").hide().fadeIn(900, "swing");
-
-});
