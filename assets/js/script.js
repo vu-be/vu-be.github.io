9850e46170e9fc3c06b79e213f0d74475cbae2c3
vu-be <vube@berkeley.edu>
2016-11-27 01:30:59 -0800

    reupload

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	var repeat = setInterval(backgroundchange, 4500);
+	var current = 1;
+	function backgroundchange() {
+		var max = 6;
+		if (current > max - 1) {
+			current = 1;
+		} else {
+			current += 1;
+		}
+		var image = "back" + current
+		var image_string = "url(assets/img/" + image + ".jpg" + ")";
+		$("#background").css('background-image', image_string)
+		console.log(image_string)
+	}
+});
---
+++ assets/js/script_guild_about.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+	$(".guild_about :not(.section_wrapper)").hide().fadeIn(700, "swing");
+
+});
